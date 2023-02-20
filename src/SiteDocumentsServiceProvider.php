5bd7c3f04b7032645a34c72c75fd344cab872b79
GIS <dev@gis4biz.ru>
2023-02-20 10:00:20 +0300

    initial commit

---
+++ src/SiteDocumentsServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Notabenedev\SiteDocuments;
+
+use Illuminate\Support\ServiceProvider;
+
+class SiteDocumentsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+
+}
